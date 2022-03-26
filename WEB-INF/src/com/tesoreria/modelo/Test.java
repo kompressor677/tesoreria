1413d6eb94a5e2e1e536c62d341ac5ab2b0366e8
kompressor677 <zerg.kompressor@gmail.com>
2022-03-26 17:49:46 -0300

    Primer Commit

---
+++ WEB-INF/src/com/tesoreria/modelo/Test.java
@@ -0,0 +1,19 @@
+package com.tesoreria.modelo;
+
+import com.doxacore.modelo.Modelo;
+
+public class Test extends Modelo{
+
+	@Override
+	public Object[] getArrayObjectDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStringDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
